7d6574c4cce8021bfd0ea96797c402550bea64b3
Voinea Radu <contact@voinearadu.com>
2024-12-02 13:15:45 +0200

    Code reformat

--- day_1/part2.py
+++ day_1/part2.py
@@ -2,7 +2,7 @@ def process(data: str):
     list_1: list[int] = [int(line.split("   ")[0]) for line in data.splitlines()]
     list_2: list[int] = [int(line.split("   ")[1]) for line in data.splitlines()]
 
-    result:int = 0
+    result: int = 0
 
     for elem in list_1:
         result += list_2.count(elem) * elem
